cd063d639cc686bd54c6644336e6af8e5dbb9418
ito110-spec <r1106-mura@yahoo.co.jp>
2025-08-04 21:07:44 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
+
+app = Flask(__name__)
+
+# 環境変数からトークンとシークレットを取得
+line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except:
+        abort(400)
+
+    return "OK"
+
+# メッセージイベント受信時の処理
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_msg = event.message.text
+    reply_msg = f"あなたが送ったメッセージ：{user_msg}"
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_msg)
+    )
+
+if __name__ == "__main__":
+    app.run()
